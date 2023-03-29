afe1f0c5b452f4cb03e4bf5a9adc7fd566e0b9bc
Vo Tat Dat <34697442+votatdat@users.noreply.github.com>
2023-03-29 23:28:13 +0700

    Finished Section 10: Data Serilization or Interceptor

---
+++ my-car-service/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export const Serialize = (dto: ClassConstructor) =>
+  UseInterceptors(new SerializeInterceptor(dto));
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
